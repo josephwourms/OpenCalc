796c75f356842c01a974f9357a772120e2784f8d
josephwourms <joseph.wourms@gmail.com>
2017-03-18 12:16:21 -0300

    make chrome window not resizable

--- background.js
+++ background.js
@@ -2,7 +2,12 @@ chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
     'outerBounds': {
       'width': 375,
-      'height': 385
-    }
+      'height': 385,
+      'maxWidth': 375,
+      'maxHeight': 385,
+      'minWidth': 375,
+      'minHeight': 385
+    },
+    'resizable': false,
   })
 })
