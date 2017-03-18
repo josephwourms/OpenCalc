c6d058ccd3bd05fcae49518aa879d542c0b3e62a
josephwourms <joseph.wourms@gmail.com>
2017-03-18 13:02:06 -0300

    refactor calcDisplayCtrl

--- js/calc-display/calc-display.controller.js
+++ js/calc-display/calc-display.controller.js
@@ -1,6 +1,14 @@
 angular.module('calcApp')
-    .controller('calcDisplayCtrl', [function() {
+    .controller('calcDisplayCtrl', ['$scope', function($scope) {
         var display = this;
         
         display.currentString = "25 \u00F7 5 = 5";
+
+        var displayElmnt = $('#display');
+        var lastElmnt    = $('<p id="last" class="displayText">Hello</p>');
+        var currentElmnt = $('<p id="current class="displayText"></p>');
+
+        currentElmnt.html(display.currentString);
+        displayElmnt.append(currentElmnt);
+        displayElmnt.append(lastElmnt);
     }]);
